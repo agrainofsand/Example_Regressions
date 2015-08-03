9bc264d2b4d9dd2ff698f219281fca545203e41d
Peishan Tan <peishan.tan@X-Peishan-Tan-MBP13.local>
2015-08-03 16:36:12 +1000

    demostrates the dials we can turn on in the rpart model, and in particular how we can overfit

--- decisiontree1.R
+++ decisiontree1.R
@@ -14,3 +14,10 @@ plot(fit)
 text(fit)
 
 fancyRpartPlot(fit)
+
+# unleashes the depth that the tree can grow, 
+# and opens up the minisplit which governs how many passengers must sit in a bucket before even looking for a split
+# demonstrates overfitting
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train,
+             method="class", control=rpart.control(minsplit=2, cp=0))
+fancyRpartPlot(fit)
